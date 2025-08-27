526c8f9437aae2f0595cbe981e2c7972214818f6
Resh19S <reshaldahima0@gmail.com>
2025-08-28 01:10:02 +0530

    ok10.5

--- project/src/components/AnswerDisplay.tsx
+++ project/src/components/AnswerDisplay.tsx
@@ -149,7 +149,7 @@ export function AnswerDisplay({ result }: AnswerDisplayProps) {
   };
 
   return (
-    <div className="py-08 bg-gray-50 dark:bg-gray-800">
+    <div className="py-8 bg-gradient-to-br from-gray-50 to-blue-50 dark:from-gray-900 dark:to-gray-800">
       <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 space-y-8">
         {/* Main Query Results */}
         <motion.section
